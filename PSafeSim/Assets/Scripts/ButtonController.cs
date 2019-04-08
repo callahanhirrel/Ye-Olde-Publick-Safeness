f8e36789bc2bae216329365dfde94a8bdb7b5d66
TaylorBarker22 <barkertt@hendrix.edu>
2019-04-08 14:00:42 -0500

    Changed main menu font, play button now loads game scene

---
+++ PSafeSim/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void ShowHowToPlay()
+    {
+        //Enable popup that shows how to play
+        //Modal window?
+    }
+}
