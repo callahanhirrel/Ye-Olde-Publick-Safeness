975e3b96dce82d9811910bbadff06bd9cb1be202
TaylorBarker22 <barkertt@hendrix.edu>
2019-04-16 09:08:58 -0500

    exchanged plane for terrain, added how to play functionality on menu

--- PSafeSim/Assets/Scripts/ButtonController.cs
+++ PSafeSim/Assets/Scripts/ButtonController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
+    public Button PlayButton;
+    public Button HowToPlayButton;
+    public Button BackToMenuButton;
+    public Text HowToPlayText;
+
     void Start()
     {
-
+        BackToMenuButton.gameObject.SetActive(false);
+        HowToPlayText.gameObject.SetActive(false);
     }
 
     public void LoadLevel(string level)
@@ -17,7 +24,19 @@ public class ButtonController : MonoBehaviour
 
     public void ShowHowToPlay()
     {
-        //Enable popup that shows how to play
-        //Modal window?
+        PlayButton.gameObject.SetActive(false);
+        HowToPlayButton.gameObject.SetActive(false);
+
+        HowToPlayText.gameObject.SetActive(true);
+        BackToMenuButton.gameObject.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        HowToPlayText.gameObject.SetActive(false);
+        BackToMenuButton.gameObject.SetActive(false);
+
+        PlayButton.gameObject.SetActive(true);
+        HowToPlayButton.gameObject.SetActive(true);
     }
 }
