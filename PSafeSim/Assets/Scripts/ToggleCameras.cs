1202fe8157865d443f6e721864cd7c5868024ff1
callahanhirrel <callahan.hirrel@gmail.com>
2019-04-22 17:36:50 -0500

    camera movement
    Press Q when zoomed to the shoulder camera to look around with the mouse

--- PSafeSim/Assets/Scripts/ToggleCameras.cs
+++ PSafeSim/Assets/Scripts/ToggleCameras.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class ToggleCameras : MonoBehaviour
 {
     private Camera mainCam;
-    public Camera zoomCam;
+    public Camera shoulderCam;
     private bool isZoomed = false;
+    private bool shoulderCamMove = false;
+    private Quaternion shoulderCamDefaultRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCam = GetComponent<Camera>();
         mainCam.enabled = true;
-        zoomCam.enabled = false;
+        shoulderCam.enabled = false;
+        shoulderCamDefaultRotation = shoulderCam.transform.rotation;
     }
 
     // Update is called once per frame
@@ -24,15 +27,41 @@ public class ToggleCameras : MonoBehaviour
             if (isZoomed == false)
             {
                 mainCam.enabled = false;
-                zoomCam.enabled = true;
+                shoulderCam.enabled = true;
                 isZoomed = true;
             }
             else
             {
                 mainCam.enabled = true;
-                zoomCam.enabled = false;
+                shoulderCam.enabled = false;
                 isZoomed = false;
+                shoulderCamMove = false;
+                shoulderCam.transform.rotation = shoulderCamDefaultRotation;
+            } 
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && isZoomed == true)
+        {
+            if (shoulderCamMove == false)
+            {
+                shoulderCamMove = true;
             }
+            else
+            {
+                shoulderCamMove = false;
+                shoulderCam.transform.rotation = shoulderCamDefaultRotation;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (shoulderCamMove == true)
+        {
+            float hitdist = 0.0f;
+            Plane plane = new Plane(Vector3.up, shoulderCam.transform.position);
+            Vector3 targetPoint = shoulderCam.ScreenPointToRay(Input.mousePosition).GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - shoulderCam.transform.position);
+            shoulderCam.transform.rotation = Quaternion.Slerp(shoulderCam.transform.rotation, targetRotation, Time.deltaTime);
         }
     }
 }
