5a92a36cdba1fcdb42261fb3daab458cb899cf5d
TaylorBarker22 <barkertt@hendrix.edu>
2019-04-23 09:32:19 -0500

    Added duel scene

---
+++ PSafeSim/Assets/Scripts/DataScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataScript
+{
+    public static Vector3 Location { get; set; }
+    public static int Shenanigans { get; set; }
+    public static float TimeLeft { get; set; }
+}
---
+++ PSafeSim/Assets/Scripts/DuelController.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DuelController : MonoBehaviour
+{
+    public Camera cam;
+    private AudioSource audioSource;
+    private float timeToEnemySpawn;
+    public float timeLimit = 10.0f - (DataScript.Shenanigans * 0.5f);
+    public GameObject squares;
+
+    void Start()
+    {
+        
+        var random = new System.Random();
+        timeToEnemySpawn = random.Next(0, 10);
+        audioSource = cam.GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            if (timeToEnemySpawn > 0) // Enemy has not spawned yet
+            {
+                timeToEnemySpawn -= Time.deltaTime;
+            }
+            else // Enemy spawn timer reaches 0
+            {
+                
+            }
+        }
+    }
+}
