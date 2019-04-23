9cf1163c71834fb84b210a4ab252d05166e061a6
TaylorBarker22 <barkertt@hendrix.edu>
2019-04-22 21:53:56 -0500

    Added countdown timer and script

---
+++ PSafeSim/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public Text text;
+    private float timeLeft = 120.0f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "Time Left:" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            //game over
+        }
+    }
+}
